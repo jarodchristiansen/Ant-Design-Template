b08b2167323d2290bd13d3b36278c93f79feaca3
Jarod Christiansen <jarodchristiansen2@gmail.com>
2021-06-25 08:45:38 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import React from "react";
+import "./App.css";
+import { Typography } from 'antd';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+
+const { Title } = Typography;
+const { SubMenu } = Menu;
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Layout>
+    <Header style={{padding: '20px'}}>
+    <Avatar style={{float: 'right'}}icon={<UserOutlined />} />
+    <Title style={{color: 'white'}} level={3}>Jarod</Title>
+    </Header>
+      <Layout>
+      <Sider>
+      <Menu
+        defaultSelectedKeys={['2']}
+        mode="inline"
+      >
+        <Menu.Item key="dashboard"> 
+          Dashboard
+        </Menu.Item>
+        <SubMenu key="sub1" title="Navigation One">
+          <Menu.ItemGroup key="g1" title="Item 1">
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup key="g2" title="Item 2">
+            <Menu.Item key="3">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </Menu>
+      </Sider>
+      <Layout>
+      <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content" style={{background: '#fff', padding: 24, minHeight: 400}}>Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design Example Created By Jarod Christiansen</Footer>
+        </Layout>
+      </Layout>
+    </Layout>
     </div>
   );
 }
